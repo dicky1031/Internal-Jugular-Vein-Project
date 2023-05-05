ead038be549512bfcb1c223c19bda1b5c1d058d1
dicky1031 <dicky10311111@gmail.com>
2023-05-06 00:25:15 +0900

    update muscle formula (with collagen based)

--- prediction_model_test_muscle_blood/S3_generate_prediction_input.py
+++ prediction_model_test_muscle_blood/S3_generate_prediction_input.py
@@ -24,21 +24,28 @@ with open(os.path.join('OPs_used', "muscle_SO2.json"), 'r') as f:
 def gen_prediction_input(num : int, train_or_test: str, muscle_SO2_used:float, SO2_used : list):
     for id in range(num):
         for blc in bloodConc:
-            prediction_input = np.empty((len(SO2_used),4*len(wavelength)+4)) # T1_large_SDS1/SDS2 T1_small_SDS1/SDS2 T2_large_SDS1/SDS2 T2_small_SDS1/SDS2 bloodConc id and muscle_change
+            prediction_input = np.zeros((len(SO2_used),4*len(wavelength)+4)) # T1_large_SDS1/SDS2 T1_small_SDS1/SDS2 T2_large_SDS1/SDS2 T2_small_SDS1/SDS2 bloodConc id and muscle_change
             for i, s in enumerate(SO2_used):
-                surrogate_result_T1 = pd.read_csv(os.path.join("dataset", "surrogate_result", train_or_test, 
-                                                            f'bloodConc_{blc}', f'muscle_SO2_{base_muscle_SO2}', 'ijv_SO2_0.7', f'{id}_{train_or_test}.csv'))
-                surrogate_result_T2 = pd.read_csv(os.path.join("dataset", "surrogate_result", train_or_test, 
-                                                            f'bloodConc_{blc}', f'muscle_SO2_{muscle_SO2_used}', f'ijv_SO2_{s}', f'{id}_{train_or_test}.csv'))
-                prediction_input[i][:20] = surrogate_result_T1['largeIJV_SDS1'] / surrogate_result_T1['largeIJV_SDS2']
-                prediction_input[i][20:40] = surrogate_result_T1['smallIJV_SDS1'] / surrogate_result_T1['smallIJV_SDS2']     
-                prediction_input[i][40:60] = surrogate_result_T2['largeIJV_SDS1'] / surrogate_result_T2['largeIJV_SDS2']
-                prediction_input[i][60:80] = surrogate_result_T2['smallIJV_SDS1'] / surrogate_result_T2['smallIJV_SDS2']
-                prediction_input[i][80] = blc
-                prediction_input[i][81] = s-0.7 # answer
-                prediction_input[i][82] = id # for analyzing used
-                prediction_input[i][83] = muscle_SO2_used-base_muscle_SO2 # muslce_mua_change
-            np.save(os.path.join("dataset", "prediction_result", train_or_test, f"{id}_blc_{blc}_muscle_change_{muscle_SO2_used-base_muscle_SO2:.2f}.npy"), prediction_input)
+                if (abs(s-0.7) < abs(muscle_SO2_used-base_muscle_SO2)) | ((muscle_SO2_used-base_muscle_SO2)*(s-0.7) < 0): # to avoid muscle SO2 change larger than IJV SO2 change
+                    continue
+                else:
+                    surrogate_result_T1 = pd.read_csv(os.path.join("dataset", "surrogate_result", train_or_test, 
+                                                                f'bloodConc_{blc}', f'muscle_SO2_{base_muscle_SO2}', 'ijv_SO2_0.7', f'{id}_{train_or_test}.csv'))
+                    surrogate_result_T2 = pd.read_csv(os.path.join("dataset", "surrogate_result", train_or_test, 
+                                                                f'bloodConc_{blc}', f'muscle_SO2_{muscle_SO2_used}', f'ijv_SO2_{s}', f'{id}_{train_or_test}.csv'))
+                    prediction_input[i][:20] = surrogate_result_T1['largeIJV_SDS1'] / surrogate_result_T1['largeIJV_SDS2']
+                    prediction_input[i][20:40] = surrogate_result_T1['smallIJV_SDS1'] / surrogate_result_T1['smallIJV_SDS2']     
+                    prediction_input[i][40:60] = surrogate_result_T2['largeIJV_SDS1'] / surrogate_result_T2['largeIJV_SDS2']
+                    prediction_input[i][60:80] = surrogate_result_T2['smallIJV_SDS1'] / surrogate_result_T2['smallIJV_SDS2']
+                    prediction_input[i][80] = blc
+                    prediction_input[i][81] = s-0.7 # answer
+                    prediction_input[i][82] = id # for analyzing used
+                    prediction_input[i][83] = muscle_SO2_used-base_muscle_SO2 # muslce_mua_change
+            row = np.where((np.zeros((1,4*len(wavelength)+4))==prediction_input).all(axis=1))
+            prediction_input_delete = np.delete(prediction_input, row, 0)
+            # print(f'origin_shape : {prediction_input.shape}, after_shape : {prediction_input_delete.shape}....')
+            
+            np.save(os.path.join("dataset", "prediction_result", train_or_test, f"{id}_blc_{blc}_muscle_change_{muscle_SO2_used-base_muscle_SO2:.2f}.npy"), prediction_input_delete)
 
 if __name__ == "__main__":
     test_num = 200
@@ -47,8 +54,7 @@ if __name__ == "__main__":
     # os.makedirs(os.path.join("dataset", "prediction_result", "train"), exist_ok=True)
     os.makedirs(os.path.join("dataset", "prediction_result", "test"), exist_ok=True)
     # gen_prediction_input(train_num, 'train', train_SO2)
-    muscle_SO2 = [i/100 for i in range(90,101,1)]
-    for muscle_SO2_used in muscle_SO2[::5]:
+    for muscle_SO2_used in muscle_SO2:
         print(f'now processing muscle_mua_change_from_{base_muscle_SO2}_to_{muscle_SO2_used}...')
         gen_prediction_input(test_num, 'test', muscle_SO2_used, test_SO2)
     
