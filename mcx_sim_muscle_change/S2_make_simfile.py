f16d48bf511c7d30b31546850a5f5a35b2aee350
dicky1031 <dicky10311111@gmail.com>
2023-06-16 14:51:39 +0900

    Update S2_make_simfile.py

--- mcx_sim_muscle_change/S2_make_simfile.py
+++ mcx_sim_muscle_change/S2_make_simfile.py
@@ -4,8 +4,8 @@ import shutil
 import json
 import numpy as np
 import sys
-result_folder = "ctchen"
-subject = "ctchen"
+result_folder = "kb"
+subject = "kb"
 PhotonNum = 1e9
 
 # %% move to current file path
