a0f8859cde6f5b877bb56736f68c5339206e8e52
dicky1031 <dicky10311111@gmail.com>
2023-07-05 16:59:35 +0800

    update ijv_size result

---
+++ run_ijv_size.sh
@@ -0,0 +1,4 @@
+python3 mcx_sim_ijv_size_change_-10%/S3_run_sim.py
+python3 mcx_sim_ijv_size_change_-20%/S3_run_sim.py
+python3 mcx_sim_ijv_size_change_-30%/S3_run_sim.py
+python3 mcx_sim_ijv_size_change_-50%/S3_run_sim.py
