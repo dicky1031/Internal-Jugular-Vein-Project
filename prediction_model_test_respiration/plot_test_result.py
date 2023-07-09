24f6cc608b2f74948865b0705ee9375a90234845
dicky1031 <dicky10311111@gmail.com>
2023-07-09 21:44:32 +0900

    plot result

--- prediction_model_test_respiration/plot_test_result.py
+++ prediction_model_test_respiration/plot_test_result.py
@@ -51,79 +51,81 @@ def convert_pvalue_to_asterisks(pvalue):
 # mus_types = ['low', 'high', 'medium']
 mus_types = ['low']
 mua_types = ['all', 'low', 'high', 'medium']
-
+# ijv_depth = ['+1mm', '+0.5mm', '-0.5mm', '-1mm', 'standard']
+ijv_depth = ['standard']
+ijv_size = ['-50%', '-30%', '-20%', '-10%', 'standard']
+# ijv_size = ['-10%']
 subject = 'ctchen'
 result = 'surrogate_formula3'
 
-for muscle_type in muscle_types:
-    for mus_type in mus_types:
-        for mua_type in mua_types:
-            
-            result_folder = os.path.join(f"{mus_type}_scatter_prediction_input_{muscle_type}", f"{mua_type}_absorption")
-            # %%
-            os.makedirs(os.path.join("pic", subject, result, result_folder), exist_ok=True)
-            test_result = pd.read_csv(os.path.join("model_test", subject, result, result_folder, "test.csv"))
+for using_depth in ijv_depth:
+    for using_size in ijv_size:
+        for mus_type in mus_types:
+            for mua_type in mua_types:
+                result_folder = os.path.join(f'ijv_depth_{using_depth}', f'ijv_size_{using_size}', f"{mus_type}_scatter_prediction_input", f"{mua_type}_absorption")
+                # %%
+                os.makedirs(os.path.join("pic", subject, result, result_folder), exist_ok=True)
+                test_result = pd.read_csv(os.path.join("model_test", subject, result, result_folder, "test.csv"))
 
-            # %%
-            muscle_SO2_dict = {}
-            for muscle_mua_change in test_result['muscle_mua_change'].unique():
-                temp = test_result[test_result['muscle_mua_change']==muscle_mua_change]
-                error = temp['error_ijv_SO2'].to_list()
-                muscle_SO2_dict[f'{muscle_mua_change}_error_ijv_SO2'] = error
-            # do t-test one-tail
-            for muscle_mua_change in test_result['muscle_mua_change'].unique():
-                statistic, pvalue = t_test(muscle_SO2_dict['0.0_error_ijv_SO2'],muscle_SO2_dict[f'{muscle_mua_change}_error_ijv_SO2'])
-                muscle_SO2_dict[f'{muscle_mua_change}_pvalue'] = pvalue
-                
-            data_y = []
-            data_x = []
-            p_val = []
-            for muscle_mua_change in sorted(test_result['muscle_mua_change'].unique(), reverse=True):
-                data_y.append(muscle_SO2_dict[f'{muscle_mua_change}_error_ijv_SO2'])
-                data_x.append(f'{muscle_mua_change:.1f}%')
-                p_val.append(muscle_SO2_dict[f'{muscle_mua_change}_pvalue'])
+                # %%
+                for muscle_mua_change in test_result['muscle_mua_change'].unique():
+                    temp = test_result[test_result['muscle_mua_change']==muscle_mua_change]
+                    error = temp['error_ijv_SO2'].to_list()
+                    muscle_SO2_dict[f'{muscle_mua_change}_error_ijv_SO2'] = error
+                # do t-test one-tail
+                for muscle_mua_change in test_result['muscle_mua_change'].unique():
+                    statistic, pvalue = t_test(muscle_SO2_dict['0.0_error_ijv_SO2'],muscle_SO2_dict[f'{muscle_mua_change}_error_ijv_SO2'])
+                    muscle_SO2_dict[f'{muscle_mua_change}_pvalue'] = pvalue
+                    
+                data_y = []
+                data_x = []
+                p_val = []
+                for muscle_mua_change in sorted(test_result['muscle_mua_change'].unique(), reverse=True):
+                    data_y.append(muscle_SO2_dict[f'{muscle_mua_change}_error_ijv_SO2'])
+                    data_x.append(f'{muscle_mua_change:.1f}%')
+                    p_val.append(muscle_SO2_dict[f'{muscle_mua_change}_pvalue'])
 
-            ax = sns.boxplot(data=data_y)
-            count = 5
-            for i in range(len(data_x)):
-                if i==5:
-                    continue
-                else:
-                    x1,x2 = 5,i
-                    y,h = max(test_result['error_ijv_SO2'])+count, 1
-                    #绘制横线位置
-                    ax.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=1, c="k") 
-                    #添加P值
-                    ax.text((x1+x2)*.5, y+h, convert_pvalue_to_asterisks(p_val[i]), ha='center', va='bottom', color="k")
-                    count += 15
-            plt.figure()
-            plt.xticks([i for i in range(len(data_y))],data_x)
-            plt.xlabel("muscle SO2 change(%)")
-            plt.ylabel("error(prediction - true)")
-            plt.savefig(os.path.join("pic", subject, result, result_folder, f"muscle_change_{muscle_mua_change}_boxplot.png"), dpi=300, format='png', bbox_inches='tight')
-            plt.close()
-            # plt.show()
-
-
-            # %%
-            for muscle_mua_change in test_result['muscle_mua_change'].unique():
-                temp = test_result[test_result['muscle_mua_change']==muscle_mua_change]
-                output = temp['output_ijv_SO2']
-                target = temp['target_ijv_SO2']
-                error = temp['error_ijv_SO2']
+                ax = sns.boxplot(data=data_y)
+                count = 5
+                for i in range(len(data_x)):
+                    if i==5:
+                        continue
+                    else:
+                        x1,x2 = 5,i
+                        y,h = max(test_result['error_ijv_SO2'])+count, 1
+                        #绘制横线位置
+                        ax.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=1, c="k") 
+                        #添加P值
+                        ax.text((x1+x2)*.5, y+h, convert_pvalue_to_asterisks(p_val[i]), ha='center', va='bottom', color="k")
+                        count += 15
                 plt.figure()
-                plt.plot(target,output, 'r.', markersize=5, label='Predict')
-                plt.plot(target,target,'b', label='Truth')
-                RMSE = np.sqrt(np.mean(np.square(error)))
-                mean = np.mean(np.abs(error))
-                std = np.std(np.abs(error))
-                max_error = np.max(np.abs(error))
-                plt.title(f"based on ijv_SO2=70% muscle_SO2_change:{muscle_mua_change}%\nmean error:{mean:.2f}% std:{std:.2f}% \nmax error:{max_error:.2f}% RMSE:{RMSE:.2f}%")
-                plt.xlabel("truth $\u0394$SO2")
-                plt.ylabel("predict $\u0394$SO2")
-                plt.legend(loc=(1.01,0.8))
-                plt.savefig(os.path.join("pic", subject, result, result_folder, f"muscle_change_{muscle_mua_change}_RMSE_ijv.png"), dpi=300, format='png', bbox_inches='tight')
+                plt.xticks([i for i in range(len(data_y))],data_x)
+                plt.xlabel("muscle SO2 change(%)")
+                plt.ylabel("error(prediction - true)")
+                plt.savefig(os.path.join("pic", subject, result, result_folder, f"muscle_change_{muscle_mua_change}_boxplot.png"), dpi=300, format='png', bbox_inches='tight')
                 plt.close()
                 # plt.show()
+
+
+                # %%
+                for muscle_mua_change in test_result['muscle_mua_change'].unique():
+                    temp = test_result[test_result['muscle_mua_change']==muscle_mua_change]
+                    output = temp['output_ijv_SO2']
+                    target = temp['target_ijv_SO2']
+                    error = temp['error_ijv_SO2']
+                    plt.figure()
+                    plt.plot(target,output, 'r.', markersize=5, label='Predict')
+                    plt.plot(target,target,'b', label='Truth')
+                    RMSE = np.sqrt(np.mean(np.square(error)))
+                    mean = np.mean(np.abs(error))
+                    std = np.std(np.abs(error))
+                    max_error = np.max(np.abs(error))
+                    plt.title(f"based on ijv_SO2=70% muscle_SO2_change:{muscle_mua_change}%\nmean error:{mean:.2f}% std:{std:.2f}% \nmax error:{max_error:.2f}% RMSE:{RMSE:.2f}%")
+                    plt.xlabel("truth $\u0394$SO2")
+                    plt.ylabel("predict $\u0394$SO2")
+                    plt.legend(loc=(1.01,0.8))
+                    plt.savefig(os.path.join("pic", subject, result, result_folder, f"muscle_change_{muscle_mua_change}_RMSE_ijv.png"), dpi=300, format='png', bbox_inches='tight')
+                    plt.close()
+                    # plt.show()
 
 
